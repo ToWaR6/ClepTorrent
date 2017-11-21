41fc6615e23fbc157cfe476468d9eadd1a1e0f15
ToWaR6 <loic.crauser@free.fr>
2017-11-21 18:09:09 +0100

    myreceiv toDo

--- etape1/server.c
+++ etape1/server.c
@@ -5,23 +5,26 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int myReceiv(int dsC, char message[]) {
+int myReceiv(int dsC) {
 	int size, res;
+	char message[10];
 	if (res = recv(dsC, &size, sizeof(int), 0) < 0) {
 		// send(dsC, -1, sizeof(int), 0);
 		perror("taille_recv()");
+		close(dsC);
 		return -1;
 	}
-
-	while (size != 0) {
+	printf("taille a recevoir : %d\n", size);
+	while (size > 0) {
 		res = recv(dsC, &message, size, 0);
 		if (res < 0) {
 			// send(dsC, -1, sizeof(int), 0);
 			perror("message_recv()");
+			close(dsC);
 			return -1;
 		}
 
-		printf("%s", message); //action to_do
+		printf("%s\n", message); //action to_do
 
 		size -= res;
 	}
@@ -34,12 +37,13 @@ int main(int argc, char const *argv[]) {
 
 	if (argc < 2) {
 		printf("%s -port\n", argv[0]);
+		return -1;
 	}
 
 	int dS = socket(AF_INET, SOCK_STREAM, 0);
 	if (dS < 0) {
 		perror("socket()");
-		exit(-1);
+		return -1;
 	}
 
 	struct sockaddr_in ad;
@@ -50,30 +54,33 @@ int main(int argc, char const *argv[]) {
 	int err = bind(dS, (struct sockaddr*)&ad, sizeof(ad));
 	if (err < 0) {
 		perror("bind()");
-		exit(-1);
+		close(dS);
+		return -1;
 	}
 
 	err = listen(dS, 2);
 	if (err < 0) {
 		perror("listen()");
-		exit(-1);
+		close(dS);
+		return -1;
 	}
 
 	struct sockaddr_in adClient;
 	socklen_t soA = sizeof(struct sockaddr_in);
 	int dSClient = accept(dS, (struct sockaddr *) &adClient, &soA) ;
 
-	char message[500];
-	int res = myReceiv(dSClient, message);
+	int res = myReceiv(dSClient);
 	if (res == -1) {
 		printf("ERREUR myReceiv\n");
+		close(dS);
+		close(dSClient);
 		return -1;
 	}
 
 
 	printf("\n");
 
-	res = send(dSClient, 0, sizeof(int), 0);
+	// res = send(dSClient, 0, sizeof(int), 0);
 
 	close(dS);
 	close(dSClient);

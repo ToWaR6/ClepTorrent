2c0fdea2e000c59385dc7d0e3bcdb39398f6461e
fdellomo <fdellomo@w27.fdsetu.infra.umontpellier.fr>
2017-11-21 15:43:16 +0100

    Main et include client.c

--- etape1/client.c
+++ etape1/client.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> //sock_addr
+#include <sys/socket.h>
+#include <arpa/inet.h> //htons, inet_pton
+#include <unistd.h> //close
+#include <errno.h>
 
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
